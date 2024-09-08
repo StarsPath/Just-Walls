433ef87a550801bee8f95c3d4d226d0b073a437b
StarsPath <liuxin.li.601@gmail.com>
2024-09-08 00:42:05 -0400

    fix wall placement bug

--- src/main/java/com/starspath/justwalls/item/WallItem.java
+++ src/main/java/com/starspath/justwalls/item/WallItem.java
@@ -70,7 +70,7 @@ public class WallItem extends StructureBlockItem {
                 }
             }
             case 2 -> { // place on side of a block
-                Direction direction = blockPlaceContext.getClickedFace();
+                Direction direction = blockPlaceContext.getHorizontalDirection();
                 for (int i = -1; i <= 1; i++) {
                     for (int j = -1; j <= 1; j++) {
                         BlockPos newPos = pos.relative(direction.getClockWise(), i).above(j);
